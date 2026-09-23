be11380b6b0c681ebc17721cdf417c133b5626f7
HIATUS <marius.thavenet@hiatusfilms.com>
2026-09-23 17:14:40 +0200

    Update databases.py

--- databases.py
+++ databases.py
@@ -16,3 +16,28 @@ def init_db():
     authdb = sqlite3.connect(auth_path)
     print(bcolors.OKGREEN + datetime.datetime.now().strftime("%H:%M:%S") + " [INFO] The database was successfully loaded" + bcolors.ENDC)
 
+    try:
+        with authdb:
+            cursor_auth = authdb.cursor()
+            sql = '''CREATE TABLE IF NOT EXISTS el_ffplayout (
+                        username TEXT,
+                        password TEXT
+            );'''
+            cursor_auth.execute(sql)
+            authdb.commit()
+    except Exception as e:
+        print(f"An error occured while creating the table:{e}")
+
+    try:
+        with authdb:
+            cursor_auth = authdb.cursor()
+            sql = '''CREATE TABLE IF NOT EXISTS el_credentials (
+                        id_client INTEGER PRIMARY KEY AUTOINCREMENT,
+                        username TEXT,
+                        password TEXT
+            );'''
+            cursor_auth.execute(sql)
+            authdb.commit()
+    except Exception as e:
+        print(f"An error occured while creating the table:{e}")
+    authdb.close()
